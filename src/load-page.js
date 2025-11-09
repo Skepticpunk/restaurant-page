5bead590e3a4447a77b05ac3f03c82c8b62a67d9
skepticpunk <14169054+Skepticpunk@users.noreply.github.com>
2025-11-09 03:39:46 -0500

    more styling, added container for article content

--- src/load-page.js
+++ src/load-page.js
@@ -4,18 +4,25 @@ const pageContent = document.querySelector("#content")
 const header = document.createElement("h1")
 const picture = document.createElement("img")
 const body = document.createElement("p")
+const caption = document.createElement("p")
 const headerContainer = document.createElement("div")
 const picContainer = document.createElement("div")
-const bodyContainer = document.createElement("div")
+const articleContainer = document.createElement("div")
 
 headerContainer.append(header)
-picContainer.appendChild(picture)
+picContainer.append(picture)
+picContainer.append(caption)
+articleContainer.append(body)
+articleContainer.append(picContainer)
 
-header.innerHTML = "Welcome to Folsom Bar and Grill!"
-headerContainer.id = "homeHeader"
+articleContainer.id = "article"
+header.innerHTML = "Welcome to the Folsom Bar & Grill!"
+headerContainer.id = "articleHeader"
 picture.src = coverImage
 picContainer.id = "coverImage"
-body.textContent = "Here at Folsom Bar and Grill, we actually give a shit about food! We want to make the most kickass food at affordable prices that we can, while providing a comfortable, enjoyable atmosphere for our guests."
+caption.textContent = "One of the many fine publications that customers enjoy at the Folsom Bar & Grill."
+caption.id = "caption"
+body.textContent = "Here at the Folsom Bar & Grill, we actually give a shit about food! We want to make the most kickass food at affordable prices that we can, while providing a comfortable, enjoyable atmosphere for our guests."
 body.id = "pageBody"
 
 class Page {
@@ -24,4 +31,4 @@ class Page {
   }
 }
 
-const mainPage = new Page(pageContent, headerContainer, body, picContainer)
+const mainPage = new Page(pageContent, headerContainer, articleContainer)
