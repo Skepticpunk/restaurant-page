2a6f9475e28c05b54225b9a3da51e08b01262e16
skepticpunk <14169054+Skepticpunk@users.noreply.github.com>
2025-11-09 03:13:55 -0500

    added some styling, changed code structure

--- src/load-page.js
+++ src/load-page.js
@@ -4,10 +4,19 @@ const pageContent = document.querySelector("#content")
 const header = document.createElement("h1")
 const picture = document.createElement("img")
 const body = document.createElement("p")
+const headerContainer = document.createElement("div")
+const picContainer = document.createElement("div")
+const bodyContainer = document.createElement("div")
+
+headerContainer.append(header)
+picContainer.appendChild(picture)
 
 header.innerHTML = "Welcome to Folsom Bar and Grill!"
+headerContainer.id = "homeHeader"
 picture.src = coverImage
+picContainer.id = "coverImage"
 body.textContent = "Here at Folsom Bar and Grill, we actually give a shit about food! We want to make the most kickass food at affordable prices that we can, while providing a comfortable, enjoyable atmosphere for our guests."
+body.id = "pageBody"
 
 class Page {
   constructor(parent, ...elements) {
@@ -15,4 +24,4 @@ class Page {
   }
 }
 
-const mainPage = new Page(pageContent, header, picture, body)
+const mainPage = new Page(pageContent, headerContainer, body, picContainer)
