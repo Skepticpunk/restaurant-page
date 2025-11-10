88ee54acfcfe336a87b13ef73b7fbb20eccd5a7e
Skepticpunk <14169054+Skepticpunk@users.noreply.github.com>
2025-11-09 19:14:06 -0500

    completed main page

--- src/load-page.js
+++ src/load-page.js
@@ -1,29 +1,25 @@
-import coverImage from "./mans-life.jpg"
+import pageImage from "./mans-life.jpg"
 
 const pageContent = document.querySelector("#content")
 const header = document.createElement("h1")
+const pageBody = document.createElement("p")
 const picture = document.createElement("img")
-const body = document.createElement("p")
 const caption = document.createElement("p")
-const headerContainer = document.createElement("div")
-const picContainer = document.createElement("div")
-const articleContainer = document.createElement("div")
+const pageHeader = document.createElement("div")
+const pageImageFrame = document.createElement("div")
 
-headerContainer.append(header)
-picContainer.append(picture)
-picContainer.append(caption)
-articleContainer.append(body)
-articleContainer.append(picContainer)
+pageHeader.append(header)
+pageImageFrame.append(picture)
+pageImageFrame.append(caption)
 
-articleContainer.id = "article"
 header.innerHTML = "Welcome to the Folsom Bar & Grill!"
-headerContainer.id = "articleHeader"
-picture.src = coverImage
-picContainer.id = "coverImage"
+pageHeader.id = "pageHeader"
+picture.src = pageImage
+pageImageFrame.id = "pageImage"
 caption.textContent = "One of the many fine publications that customers enjoy at the Folsom Bar & Grill."
 caption.id = "caption"
-body.textContent = "Here at the Folsom Bar & Grill, we actually give a shit about food! We want to make the most kickass food at affordable prices that we can, while providing a comfortable, enjoyable atmosphere for our guests."
-body.id = "pageBody"
+pageBody.textContent = "Here at the Folsom Bar & Grill, we actually give a shit about food! We want to make the most kickass food at affordable prices that we can, while providing a comfortable, enjoyable atmosphere for our guests."
+pageBody.id = "pageBody"
 
 class Page {
   constructor(parent, ...elements) {
@@ -31,4 +27,4 @@ class Page {
   }
 }
 
-const mainPage = new Page(pageContent, headerContainer, articleContainer)
+const mainPage = new Page(pageContent, pageHeader, pageBody, pageImageFrame)
