3961620fb34ba630b0771c7f8d376211122cdef0
skepticpunk <14169054+Skepticpunk@users.noreply.github.com>
2025-11-09 20:48:07 -0500

    made proper Page class

--- src/load-page.js
+++ src/load-page.js
@@ -1,30 +1,47 @@
 import pageImage from "./mans-life.jpg"
 
-const pageContent = document.querySelector("#content")
-const header = document.createElement("h1")
-const pageBody = document.createElement("p")
-const picture = document.createElement("img")
-const caption = document.createElement("p")
-const pageHeader = document.createElement("div")
-const pageImageFrame = document.createElement("div")
-
-pageHeader.append(header)
-pageImageFrame.append(picture)
-pageImageFrame.append(caption)
-
-header.innerHTML = "Welcome to the Folsom Bar & Grill!"
-pageHeader.id = "pageHeader"
-picture.src = pageImage
-pageImageFrame.id = "pageImage"
-caption.textContent = "One of the many fine publications that customers enjoy at the Folsom Bar & Grill."
-caption.id = "caption"
-pageBody.textContent = "Here at the Folsom Bar & Grill, we actually give a shit about food! We want to make the most kickass food at affordable prices that we can, while providing a comfortable, enjoyable atmosphere for our guests."
-pageBody.id = "pageBody"
-
 class Page {
-  constructor(parent, ...elements) {
-    elements.forEach((element) => parent.append(element))
-  }
-}
+  #pageContent = document.querySelector("#content")
+  #pageHeader = document.createElement("div")
+  #pageBody = document.createElement("p")
+  #pageImageFrame = document.createElement("div")  
+  #header = document.createElement("h1")
+  #image = document.createElement("img")
+  #caption = document.createElement("p")
+  
+  constructor() {
+    this.#header.innerHTML = "No page header set - use Page.setHeader()!"
+    this.#pageBody.textContent = "No page body set - use Page.setBody()!"
+    this.#caption.textContent = "No caption set - use Page.setCaption()!" }
+    
+  setHeader( newHeader ) {
+    this.#header.innerHTML = newHeader }
+  setBody( newBody ) {
+    this.#pageBody.textContent = newBody }
+  setImage( newImage ) {
+    this.#image.src = newImage }
+  setCaption( newCaption ) {
+    this.#caption.textContent = newCaption }
+  buildPage( parent ) {
+    parent.append( this.#pageHeader )
+    parent.append( this.#pageBody )
+    parent.append( this.#pageImageFrame )
+    this.#pageHeader.append( this.#header )
+    this.#pageHeader.id = "pageHeader"
+    this.#pageBody.id = "pageBody"
+    this.#pageImageFrame.append( this.#image )
+    this.#pageImageFrame.append( this.#caption )
+    this.#pageImageFrame.id = "pageImage"
+    this.#caption.id = "caption" } }
 
-const mainPage = new Page(pageContent, pageHeader, pageBody, pageImageFrame)
+const mainPage = new Page()
+
+mainPage.setHeader( "Welcome to the Folsom Bar & Grill!" )
+mainPage.setImage( pageImage )
+mainPage.setBody( "Here at the Folsom Bar & Grill, we actually give a shit about food! We want to make the most kickass food at affordable prices that we can, while providing a comfortable, enjoyable atmosphere for our guests." )
+mainPage.setCaption( "One of the many fine publications that customers enjoy at the Folsom Bar & Grill." )
+
+mainPage.buildPage( document.querySelector( "#content" ) )
+
+
+
